89b31d7fa044dced2b8b46c45ea121ad69042285
tianshi xie <187132225xts@gmail.com>
2018-05-24 23:01:33 -0500

    Add files via upload

---
+++ Assets/Scripts/CharacterLook.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLook : MonoBehaviour {
+
+    public float sensitivity;
+    public Transform characterBody;
+    public float xClamp = 0;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RotateCamera();
+
+    }
+
+    void RotateCamera()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        float rX = x * sensitivity;
+        float rY = y * sensitivity;
+
+        xClamp -= rY;
+
+        Vector3 tarCam = transform.rotation.eulerAngles;
+        Vector3 tarBody = characterBody.rotation.eulerAngles;
+
+        tarCam.x -= rY;
+        tarBody.y += rX;
+
+        if (xClamp > 90)
+        {
+            xClamp = 90;
+            tarCam.x = 90;
+        }
+        else if (xClamp < -90)
+        {
+            xClamp = -90;
+            tarCam.x = -90;
+        }
+
+        transform.rotation = Quaternion.Euler(tarCam);
+        characterBody.rotation = Quaternion.Euler(tarBody);
+    }
+}
---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+    CharacterController pController;
+    public float moveSpeed;
+    private void Awake()
+    {
+        pController = GetComponent<CharacterController>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCharacter();
+	}
+
+    void MoveCharacter()
+    {
+        float movhoriz = Input.GetAxis("Horizontal");
+        float movvert = Input.GetAxis("Vertical");
+
+        Vector3 moveDirSide = transform.right * movhoriz * moveSpeed;
+        Vector3 moveDirForward = transform.forward * movvert * moveSpeed;
+
+        pController.SimpleMove(moveDirSide);
+        pController.SimpleMove(moveDirForward);
+    }
+}
